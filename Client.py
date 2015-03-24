1a1b3245fdaff67a8de75519f020d2ef30298172
Urauth <pee-94@hotmail.com>
2015-03-24 18:58:48 +0200

    Tämä tekee jo jotain. TCP toimii. UDP ei

--- Client.py
+++ Client.py
@@ -1,9 +1,28 @@
 import socket
+import struct
 
-socket = socket.socket()
+TCPsocket = socket.socket()
 host = "ii.virtues.fi"
 port = 10000
 
-socket.connect((host, port))
-print socket.recv(1024)
-socket.close
+TCPsocket.connect((host, port))
+TCPsocket.settimeout(5)
+TCPsocket.send("HELO 10000\r\n")
+print TCPsocket.recv(1024)
+
+
+UDPsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDPsocket.settimeout(5) #sulkee sukatin jos ei saa pakettia
+
+endOfMessage = True
+acknowledgeReceipt = True
+length = 64
+message = "Bring out your dead!"
+
+#struct.pack("!??Ha", endOfMessage, acknowledgeReceipt, length)
+
+
+UDPsocket.sendto(message, (host, port));
+print UDPsocket.recv(1024)
+UDPsocket.close()
+TCPsocket.close()
