e33318f8e044b1a759983bac6772c54329290128
Kivike <r_rajala@yahoo.com>
2015-03-30 21:52:58 +0300

    Nyt toimii UDP. Eka viesti tulee.

--- Client.py
+++ Client.py
@@ -1,28 +1,53 @@
 import socket
 import struct
+import sys
+
 
 TCPsocket = socket.socket()
 host = "ii.virtues.fi"
-port = 10000
+TCPport = 10000
+UDPbindPort = 9000
+UDPsendPort = None
 
-TCPsocket.connect((host, port))
+if (len(sys.argv) >= 2):
+	host = sys.argv[1]
+
+if (len(sys.argv) >= 3):
+	TCPport = int(sys.argv[2])
+	
+if (len(sys.argv) == 4): 
+	UDPbindPort = int(sys.argv[3])
+
+print "Creating a TCP connection to %s:%d" % (host, TCPport)
+TCPsocket.connect((host, TCPport))
 TCPsocket.settimeout(5)
-TCPsocket.send("HELO 10000\r\n")
-print TCPsocket.recv(1024)
+TCPsocket.send("HELO %d\r\n" % UDPbindPort)
+TCPmsg = TCPsocket.recv(1024)
+TCPsocket.close()
+print "HOST:", repr(TCPmsg)
 
+UDPsendPort = int(filter(lambda x: x.isdigit(), TCPmsg))
+
+print "=> using UDP port", UDPsendPort, "to send datagrams."
 
 UDPsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDPsocket.settimeout(5) #sulkee sukatin jos ei saa pakettia
+UDPsocket.bind(("0.0.0.0", UDPbindPort))
 
-endOfMessage = True
-acknowledgeReceipt = True
+print "Listening UDP on port", UDPbindPort
+
+UDPsocket.settimeout(10) #sulkee sukatin jos ei saa pakettia
+
+eom = True
+ack = True
 length = 64
-message = "Bring out your dead!"
+remaining = 0
+msg = "Bring out your dead!"
+data = struct.pack('!??HH64s', eom, ack, length, remaining, msg)
 
-#struct.pack("!??Ha", endOfMessage, acknowledgeReceipt, length)
+UDPsocket.sendto(data, (host, UDPsendPort));
+print "Sent initial message, waiting for a question"
 
-
-UDPsocket.sendto(message, (host, port));
-print UDPsocket.recv(1024)
+while True:
+	print "HOST:", UDPsocket.recv(1024)
+	
 UDPsocket.close()
-TCPsocket.close()
