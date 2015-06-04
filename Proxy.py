c52d9a151b85de59ad0f213563221da5252cb16e
Kivike <r_rajala@yahoo.com>
2015-06-05 00:12:18 +0300

    Added comment section at the top. Made code clearer

--- Proxy.py
+++ Proxy.py
@@ -1,3 +1,15 @@
+# Introduction to Internet 2015
+# Roope Rajala
+# Peetu Nuottajärvi
+# Samuel Savikoski
+############################################################
+# This proxy is a stand-alone program                      #
+# There are no arguments, if you wish to change serverAddr #
+# of the proxy, edit serverAddr variable below.            #
+# Accepts ONE client connection and forwards all messages  #
+# between server and client.                               #
+############################################################
+
 import socket
 import struct
 import sys
@@ -5,7 +17,8 @@ import sys
 serverAddr = socket.gethostbyname("ii.virtues.fi")
 serverPort = 10000
 
-
+# Select which ports to bind
+# Tries all ports in range 10000-10099
 def findavailableports():
     print "Finding available ports"
 
@@ -33,27 +46,26 @@ def findavailableports():
 
     return (TCPport, UDPport)
 
-def closeconnections():
-    print "Closing connections and sockets"
-    UDPsocket.close()
-    clientConn.close()
-
+# Forward message from server to client
+# Returns End Of Message flag
 def forwardtoclient(data, clientUDPport):
     eom, ack, length, remaining, msg = struct.unpack("!??HH64s", data)
     UDPsocket.sendto(data, (clientAddr, clientUDPport))
     print "Forwarded packet to the client"
     return eom
 
+# Forward message from client to server
+# Returns End Of Message flag
 def forwardtoserver(data, serverUDPport):
     eom, ack, length, remaining, msg = struct.unpack("!??HH64s", data)
     UDPsocket.sendto(data, (serverAddr, serverUDPport))
     print "Forwarded packet to the server."
     return eom
 
-def initconnections(msg):
-    # Send TCP message to server to request UDP port
-    # Send own UDP bind port to the client
-    global clientConn, serverConn, serverUDPport, serverPort, serverAddr
+# Send TCP message to server to request an UDP port
+# Send own UDP bind port to the client
+def initconnections(msg, UDPbindPort, clientconn):
+    global serverPort, serverAddr
 
     splitMsg = msg.split()
 
@@ -74,46 +86,46 @@ def initconnections(msg):
 
     serverport = int(filter(lambda x: x.isdigit(), msgFromServer))
     clientConn.send("HELO %d\r\n" % UDPbindPort)
+    clientConn.close()
 
     return (clientport, serverport)
 
-TCPsocket = socket.socket()
-UDPsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-TCPbindPort, UDPbindPort = findavailableports()
+if "__name__" == "__main__":
+    TCPsocket = socket.socket()
+    UDPsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    TCPbindPort, UDPbindPort = findavailableports()
 
-TCPsocket.listen(1)
+    # Listen TCP socket for client
+    TCPsocket.listen(1)
+    print "Waiting for connection"
+    clientConn, addrWithPort = TCPsocket.accept()
+    TCPsocket.close()
 
-print "Waiting for connection"
-clientConn, addrWithPort = TCPsocket.accept()
+    clientAddr = addrWithPort[0]
+    print "Client connected from", clientAddr
 
-TCPsocket.close()
+    msgFromClient = clientConn.recv(1024)
+    print repr(msgFromClient)
+    clientUDPport, serverUDPport = initconnections(msgFromClient, UDPbindPort, clientConn)
 
-clientAddr = addrWithPort[0]
-print "Client connected from", clientAddr
+    if clientUDPport == None:
+        sys.exit("Wrong message format from client. Needs to be 'HELO portnumber\r\n'")
 
-msgFromClient = clientConn.recv(1024)
-print repr(msgFromClient)
+    forwarding = True
+    while(forwarding):
+        data, addrWithPort = UDPsocket.recvfrom(1024)
+        print "Received UDP packet from", addrWithPort[0]
 
-clientUDPport, serverUDPport = initconnections(msgFromClient, UDPbindPort)
+        if addrWithPort[0] == clientAddr:
+            print "Received packet from the client."
+            if forwardtoserver(data, serverUDPport) == True:
+                forwarding = False
+        elif addrWithPort[0] == serverAddr:
+            print "Received packet from the server."
+            if forwardtoclient(data, clientUDPport) == True:
+                forwarding = False
 
-if clientUDPport == None:
-    sys.exit("Wrong message format from client. Needs to be 'HELO portnumber\r\n'")
-
-forwarding = True
-while(forwarding):
-    data, addrWithPort = UDPsocket.recvfrom(1024)
-    print "Received UDP packet from", addrWithPort[0]
-
-    if addrWithPort[0] == clientAddr:
-        print "Received packet from the client."
-        if forwardtoserver(data, serverUDPport) == True:
-            forwarding = False
-    elif addrWithPort[0] == serverAddr:
-        print "Received packet from the server."
-        if forwardtoclient(data, clientUDPport) == True:
-            forwarding = False
-
-closeconnections()
+    UDPsocket.close()
 
 
 
--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 Roope Rajala 2374556 roope.rajala@student.oulu.fi
-Peetu Nuottajärvi
+Peetu Nuottajärvi 2374491 peetu.nuottajarvi@student.oulu.fi
 Samuel Savikoski
 
 
